4daca5100818fa650f779ef39351a24a852fadb7
Rafael Falaschi <rafinhabraga2324@gmail.com>
2024-06-25 18:58:25 -0300

    curriculo commit

--- teste.php
+++ teste.php
@@ -51,7 +51,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Dados Inseridos</title>
-    <link rel="stylesheet" href="estilo.css" />
+    <link rel="stylesheet" href="php.css" />
 </head>
 <body>
     <main id="container">
